1bd1760c81782d175f4478343e108b3d27becb0f
w3schools-test <test@w3schools.com>
2022-03-09 11:57:17 +0100

    7 files added

---
+++ demo_http_url.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    //Return the url part of the request object:
+    res.write(req.url);
+    res.end();
+  })
+  .listen(8080);
